4f8f9a2d66a72364ed1ed532fb3d8d0aca607cc1
Dina Rajaonson <d.rajaonson@gmail.com>
2025-03-12 22:41:11 +0400

    URL : select programme date

--- programme/views.py
+++ programme/views.py
@@ -4,11 +4,14 @@ from django.utils import timezone
 from .models import Programme
 
 
-def index(request, template='programme.html'):
+def index(request, date=None):
     today = timezone.now().date()
 
-    # Récupérer le programme futur le plus proche
-    programme = Programme.objects.filter(is_active=True, date__gte=today).order_by('date').first()
+    if date is not None:
+        programme = Programme.objects.filter(date=date).first()
+    else:
+        # Récupérer le programme futur le plus proche
+        programme = Programme.objects.filter(is_active=True, date__gte=today).order_by('date').first()
 
     # Si aucun programme futur n'existe, récupérer le dernier programme passé
     if not programme:
@@ -17,6 +20,6 @@ def index(request, template='programme.html'):
     if programme:
         programme.nb_views += 1
         programme.save()
-        return render(request, template, {'programme': programme})
+        return render(request, 'programme.html', {'programme': programme})
 
     return render(request, 'not_found.html')
