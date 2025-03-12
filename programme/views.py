9b6e94e41f23f879df4b853ed746021477ef4fa0
Dina Rajaonson <d.rajaonson@gmail.com>
2025-03-12 22:29:20 +0400

    feat: afficher le programme futur le plus proche ou le dernier disponible
    - Sélectionne le programme dont la date est la plus proche à partir d'aujourd'hui
    - Si aucun programme futur n'est trouvé, affiche le dernier programme passé
    - Met à jour le compteur de vues du programme affiché
    - Garantit qu'un programme est toujours affiché si disponible

--- programme/views.py
+++ programme/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 from .models import Programme
 
 
 def index(request, template='programme.html'):
-    programme = Programme.objects.filter(is_active=True).first()
+    today = timezone.now().date()
+
+    # Récupérer le programme futur le plus proche
+    programme = Programme.objects.filter(is_active=True, date__gte=today).order_by('date').first()
+
+    # Si aucun programme futur n'existe, récupérer le dernier programme passé
+    if not programme:
+        programme = Programme.objects.filter(is_active=True, date__lte=today).order_by('-date').first()
 
     if programme:
-        programme.nb_views = programme.nb_views + 1
+        programme.nb_views += 1
         programme.save()
         return render(request, template, {'programme': programme})
 
